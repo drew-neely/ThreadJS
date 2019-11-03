f0b2bd02f85bf397eff949d4b1466d63b29321c2
Drew Neely <gpsdrew@gmail.com>
2019-11-03 01:02:06 -0500

    Implemented serialization

--- cppsrc/main.cpp
+++ cppsrc/main.cpp
@@ -9,23 +9,25 @@
 using namespace Napi;
 using namespace std;
 
-Value add(const CallbackInfo& info) {
-    Env env = info.Env();
-    double sum = 0;
-    for(int i = 0; i < info.Length(); i++) {
-        if(info[i].IsNumber()) {
-            sum += info[i].ToNumber().DoubleValue();
-        } else {
-            // fail
-        }
-    }
-    napi_value result;
-    napi_status status = napi_create_double(env, sum, &result);
-    return Value(env, result);
-}
 
 void childResolve(const CallbackInfo& info) {
-
+    Env env = info.Env();
+    Function serialize = env.Global().Get("serialize").As<Function>();
+    if(info.Length() == 1) {
+        napi_value data = (napi_value) info[0];
+        Value serializedData = serialize.Call(1, &data);
+        assert(serializedData.IsBuffer());
+        Buffer<uint8_t> buf = serializedData.As<Buffer<uint8_t>>();
+        size_t len = buf.Length();
+        uint8_t* bufData = buf.Data();
+        cout << "len = " << len << endl;
+        for(int i = 0; i < len; i++) {
+            cout << (int) bufData[i] << " ";
+        }
+        cout << endl;
+    } else {
+        assert(false);
+    }
 }
 
 void childReject(const CallbackInfo& info) {
@@ -42,21 +44,38 @@ Value makeThread(const CallbackInfo& info) {
                 (napi_value)Function::New(env, childReject)
             };
             Value val = info[0].As<Function>().Call(2, args);
-            
-            exit(0);
-        } else {
+        } else { // parent
             napi_value res;
             napi_create_uint32(env, pid, &res);
-            return Value(env, res);
+            return Value(env, res); 
         }
     } else {
         //fail
     }
 }
 
+// void registerSerial(const CallbackInfo& info) {
+//     Env env = info.Env();
+//     if(info.Length() == 3 && info[0].IsFunction() && info[1].IsFunction()) {
+//         serialize = (Function*) malloc(sizeof(Function));
+//         deserialize = (Function*) malloc(sizeof(Function));
+//         *serialize = info[0].As<Function>();
+//         *deserialize = info[1].As<Function>();
+//         napi_value data = (napi_value) info[2];
+//         Value serializedData = serialize->Call(1, &data);
+//         cout << "rs data buf = " << serializedData.IsBuffer() << endl;
+//         cout << "buf length = " << serializedData.As<Buffer<char>>().Length() << endl;
+//     } else {
+//         cout << info.Length() << endl;
+//         cout << info[0].IsFunction() << endl;
+//         cout << info[1].IsFunction() << endl;
+//         assert(false); // incorrect call to registerSerial
+//     }
+// }
+
 Object Init(Env env, Object exports) {
-    exports.Set(String::New(env, "add"), Function::New(env, add));
     exports.Set(String::New(env, "makeThread"), Function::New(env, makeThread));
+    // exports.Set(String::New(env, "registerSerial"), Function::New(env, registerSerial));
     return exports;
 }
 
--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 //index.js
-const ThreadJS = require('./build/Release/ThreadJS.node');
+var ThreadJS = require('./build/Release/ThreadJS.node');
 var Promise = require('promise');
+var { serialize, deserialize } = require('v8');
+global.serialize = serialize;
+global.deserialize = deserialize;
 
-var makeThread = ThreadJS.makeThread;
+process.on('SIGINT', () => {
 
-const sleep = (milliseconds) => {
+});
+
+var sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
 class Thread extends Promise {
     constructor(work) {
-        super((reolve, reject) => {
-            makeThread(work, resolve, reject);
+        super((resolve, reject) => {
+            ThreadJS.makeThread(work, resolve, reject);
         });
     }
 }
 
 var thread = new Thread((resolve, reject) => {
     console.log("I'm running!");
-    sleep(5000).then(() => console.log("Done"));
+    resolve(1000);
 });
+
+
+sleep(1000).then(() => console.log("Done"));
 
 module.exports = ThreadJS;
